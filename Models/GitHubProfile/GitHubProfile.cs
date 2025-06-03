a5e269f41911d8bda2ad8cf9f3553147cf0ee7f2
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-06-03 14:08:34 +0200

    model for user's profile

---
+++ Models/GitHubProfile/GitHubProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace GitHubApiCall.Models.GitHubProfile
+{
+    internal class GitHubProfile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string Email { get; set; }
+        public string TwitterUsername { get; set; }
+        public int Followers { get; set; }
+        public int Following { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
