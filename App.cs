de9869eba90be8dcceef5e9612a76179b8575599
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-06-03 13:26:04 +0200

    dependency injection moved out of main

---
+++ App.cs
@@ -0,0 +1,85 @@
+﻿using GitHubApiCall.Helpers;
+using GitHubApiCall.Interfaces;
+using Spectre.Console;
+
+namespace GitHubApiCall
+{
+    internal class App(IApiCallService apiCallService)
+    {
+        public void Run()
+        {
+            Helper.WelcomeMessage();
+
+            while (true)
+            {
+                Helper.PrintCommandMessage("Enter command : ");
+                var input = Console.ReadLine() ?? string.Empty;
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Helper.PrintWarningMessage("No input detected, try again !");
+                    continue;
+                }
+
+                switch (input)
+                {
+                    case "help":
+                        HelpMessage();
+                        break;
+                    case "get-events":
+                        PrintUserEvents();
+                        break;
+                    case "exit":
+                        return;
+                }
+            }
+        }
+
+        private void PrintUserEvents()
+        {
+            var username = Helper.PromptForUsername();
+            if (username == null) return;
+
+            AnsiConsole.Write(new Rule($"[yellow]GitHub Events for [bold]{username}[/bold][/yellow]").RuleStyle("grey"));
+
+            var events = apiCallService.GetUserEventsAsync(username).Result;
+
+            if (events == null || events.Count == 0)
+            {
+                AnsiConsole.MarkupLine("[italic red]No events to display.[/]");
+                return;
+            }
+
+            foreach (var element in events)
+            {
+                var message = element.Type switch
+                {
+                    "PushEvent" => $"- Pushed [green]{element.Payload?.Commits?.Count ?? 0}[/] commit(s) to [blue]{element.Repo.Name}[/]",
+                    "IssuesEvent" when element.Payload?.Action == "opened" => $"- Opened a new issue in [blue]{element.Repo.Name}[/]",
+                    "WatchEvent" when element.Payload?.Action == "started" => $"- Starred [blue]{element.Repo.Name}[/]",
+                    _ => string.Empty
+                };
+
+                if (!string.IsNullOrEmpty(message))
+                    AnsiConsole.MarkupLine(message);
+            }
+        }
+
+        static void HelpMessage()
+        {
+            var count = 1;
+            var list = new List<string>
+            {
+                "get-events - To list events for given user.",
+                "exit - To close the app",
+                "clear - To clear console window"
+            };
+
+            foreach (var element in list)
+            {
+                Console.WriteLine($"{count} : {element}");
+                count++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using GitHubApiCall.Helpers;
-using GitHubApiCall.Interfaces;
-using GitHubApiCall.Models;
+﻿using GitHubApiCall.Interfaces;
 using GitHubApiCall.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using Spectre.Console;
 
 namespace GitHubApiCall
 {
@@ -12,92 +8,13 @@ namespace GitHubApiCall
     {
         static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection().AddSingleton<IApiCallService, ApiCallService>().BuildServiceProvider();
-            var apiCallService = serviceProvider.GetService<IApiCallService>();
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IApiCallService, ApiCallService>()
+                .AddSingleton<App>()
+                .BuildServiceProvider();
 
-            Helper.WelcomeMessage();
-
-            while (true)
-            {
-                Helper.PrintCommandMessage("Enter command : ");
-
-                var input = Console.ReadLine() ?? string.Empty;
-
-                if (string.IsNullOrEmpty(input))
-                {
-                    Helper.PrintWarningMessage("No input detected, try again !");
-                }
-
-                var exit = false;
-
-                switch(input)
-                {
-                    case "help":
-                        HelpMessage();
-                        break;
-                    case "get-events":
-                        PrintUserEvents();
-                        break;
-                    case "exit":
-                        exit = true;
-                        break;
-                }
-
-                if (exit)
-                {
-                    break;
-                }
-            }
-
-            void PrintUserEvents()
-            {
-                var username = Helper.PromptForUsername();
-                if (username == null)
-                    return;
-
-                AnsiConsole.Write(new Rule($"[yellow]GitHub Events for [bold]{username}[/][/]").RuleStyle("grey"));
-
-                var events =  apiCallService?.GetUserEventsAsync(username).Result;
-
-                if (events == null || events.Count == 0)
-                {
-                    AnsiConsole.MarkupLine("[italic red]No events to display.[/]");
-                    return;
-                }
-
-                foreach (var element in events)
-                {
-                    var message = element.Type switch
-                    {
-                        "PushEvent" =>
-                            $"- Pushed [green]{element.Payload?.Commits?.Count ?? 0}[/] commit(s) to [blue]{element.Repo.Name}[/]",
-                        "IssuesEvent" when element.Payload?.Action == "opened" =>
-                            $"- Opened a new issue in [blue]{element.Repo.Name}[/]",
-                        "WatchEvent" when element.Payload?.Action == "started" => $"- Starred [blue]{element.Repo.Name}[/]",
-                        _ => string.Empty
-                    };
-            
-                    if (!string.IsNullOrEmpty(message))
-                        AnsiConsole.MarkupLine(message);
-                }
-            }
-        }
-
-        static void HelpMessage()
-        {
-            var count = 1;
-            var list = new List<string>
-            {
-                "get-events - To list events for given user.",
-                "exit - To close the app",
-                "clear - To clear console window"
-            };
-
-            foreach (var element in list)
-            {
-                Console.WriteLine($"{count} : {element}");
-                count++;
-            }
+            var app = serviceProvider.GetRequiredService<App>();
+            app.Run();
         }
     }
 }
