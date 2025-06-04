f4012a063a62f9159d693dc112915fae009c5bf0
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-06-04 13:00:21 +0200

    modify user profile model

--- Models/GitHubProfile/GitHubProfile.cs
+++ Models/GitHubProfile/GitHubProfile.cs
@@ -2,16 +2,10 @@
 {
     internal class GitHubProfile
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Company { get; set; }
         public string Location { get; set; }
-        public string Email { get; set; }
-        public string TwitterUsername { get; set; }
         public int Followers { get; set; }
         public int Following { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-
     }
 }
